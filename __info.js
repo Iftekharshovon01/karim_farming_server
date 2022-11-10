887d514b7d25adb866aa403f68a67f88b3998703
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2022-11-11 01:27:17 +0600

    second commit

--- __info.js
+++ __info.js
@@ -1,2 +1,11 @@
-// databseUserNAme:dbFarming1
-// dbPAssword: yPQjvDR9oPpu3M3o
+// // databseUserNAme:dbFarming1
+// // dbPAssword: yPQjvDR9oPpu3M3o
+
+
+// const uri = "mongodb+srv://dbFarming1:<password>@cluster0.ire9p3f.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the
+//     client.close();
+// });
--- index.js
+++ index.js
@@ -4,10 +4,4 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
-const uri = "mongodb+srv://dbFarming1:<password>@cluster0.ire9p3f.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
