9c65af9d5dd199ca61bec9a7263b5bba7b905499
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2022-11-11 11:44:18 +0600

    basic node

--- __info.js
+++ __info.js
@@ -2,10 +2,3 @@
 // // dbPAssword: yPQjvDR9oPpu3M3o
 
 
-// const uri = "mongodb+srv://dbFarming1:<password>@cluster0.ire9p3f.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the
-//     client.close();
-// });
--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ire9p3f.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('karim farming is runnning')
+})
+
+
+app.listen(port, () => { console.log(`karim farming is running on ${port}`); });
+
+
 
 
 
